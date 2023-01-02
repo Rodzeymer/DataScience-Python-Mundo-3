8a572fe508dd11a1999c43f03aa770291f515207
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-02 00:36:50 -0300

    75 corrigido

--- Python Mundo 3- Desafio 75.py
+++ Python Mundo 3- Desafio 75.py
@@ -8,10 +8,14 @@ for c in range (0,4):
 tupla = (lista[0], lista[1], lista[2], lista[3])
 quantosNove = tupla.count(9)
 if 3 not in tupla:
-    primeiroTres = 0
+    primeiroTres = 'Não há tres'
 else:
     primeiroTres = tupla.index(3)
 print(f'Os números sorteados foram: {tupla}')
 print(f'Apareceram {quantosNove} números 9')
-print(f'O primeiro 3 apaarece na posição {primeiroTres}')
+if 3 not in tupla:
+    print('Não há número três')
+else:
+    primeiroTres = tupla.index(3)
+    print(f'O primeiro 3 aparece na posição {primeiroTres}')
 print(f'Foram digitados {numerosPar} números pares')
