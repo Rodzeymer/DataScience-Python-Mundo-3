9f07c986fea6363c9f498d9069dac3546eb53d0a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-02 00:46:46 -0300

    74 melhorado

--- Python Mundo 3- Desafio 74.py
+++ Python Mundo 3- Desafio 74.py
@@ -3,7 +3,7 @@ lista = []
 for c in range (0,5):
     lista.append(randint(0,10))
 tupla = (lista[0], lista[1], lista[2], lista[3], lista[4])
-valores = sorted(tupla)
+
 print(f'Os números sorteados foram: {tupla}')
-print(f'O maior número sorteado foi: {valores[-1]}')
-print(f'O menor número sorteado foi: {valores[0]}')
+print(f'O maior número sorteado foi: {max(tupla)}')
+print(f'O menor número sorteado foi: {min(tupla)}')
