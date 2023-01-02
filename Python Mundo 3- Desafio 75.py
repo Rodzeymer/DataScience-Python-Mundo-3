8002fc3f562eceac308bf60b93e8e42179abb19a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-02 00:39:45 -0300

    tupla

--- Python Mundo 3- Desafio 75.py
+++ Python Mundo 3- Desafio 75.py
@@ -1,9 +1,11 @@
 lista = []
 numerosPar = 0
+listaPar =[]
 for c in range (0,4):
     numeroNovo = int(input(f'Digite o {c+1}º número'))
     if numeroNovo %2 == 0:
         numerosPar = numerosPar+1
+        listaPar.append(numeroNovo)
     lista.append(numeroNovo)
 tupla = (lista[0], lista[1], lista[2], lista[3])
 quantosNove = tupla.count(9)
@@ -17,5 +19,6 @@ if 3 not in tupla:
     print('Não há número três')
 else:
     primeiroTres = tupla.index(3)
-    print(f'O primeiro 3 aparece na posição {primeiroTres}')
+    print(f'O primeiro 3 aparece na posição {primeiroTres+1}')
 print(f'Foram digitados {numerosPar} números pares')
+print(f'Os números pares digitados foram {listaPar}')
