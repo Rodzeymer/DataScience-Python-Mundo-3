de506ef6648b4c9aa4bd2b19a5d798b01e1003e3
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-17 14:36:45 -0300

    Desafio 73 refeito, melhorado e corrigido!

--- Python Mundo 3- Desafio 73.py
+++ Python Mundo 3- Desafio 73.py
@@ -1,3 +1,17 @@
+# Neste desafio vamos pegar a tabela do Brasileirão, aqui o de 2022, inserir os times em ordem
+# de classificação e dar algumas opções para o usuário possa interagir, como a de exibir os 
+# 5 primeiros lugares, ou os 4 últimos, os times em ordem alfabética e qual a posição do Ceará
+
+# O algortimo pega a tupla com os times, já que a tupla é imutável e tem que ser criada antes
+# da execução e dependendo da opção escolhida vai ser xecutada uma das funções, de fatiamento, 
+# pegando os 5 primeiros ou os 4 últimos elementos, organizar os elementos ou verificar a
+# primeira ocorrência do termo 'Ceará', ou se for insarido uma opção que não essas quatro o 
+# programa retorna uma mensagem de opção inválida.
+
+print('*' *30)
+print(f"{'Python Mundo 3 - Desafio 73' :^30}")
+print('*' *30)
+
 tabela = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athlético-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goiás', 'Red Bull Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avaí', 'Juventude')
 
 print('<>'*20)
@@ -10,7 +24,6 @@ escolha = int(input('''Digite a sua escolha
 3 - Os times em ordem alfabética;
 4 - A posição do Ceará;'''))
 
-
 if escolha == 1:
     print(f'Os 5 primeiros colocados são: {tabela[0:5]}')
 elif escolha == 2:
@@ -21,3 +34,7 @@ elif escolha == 4:
     print(f'O Ceará está na {tabela.index("Ceará")+1}ª posição')
 else:
     print('Opção inválida')
+    
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
