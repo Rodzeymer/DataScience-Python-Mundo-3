436aafc816d927e07cdb04d1c95f2f39def38e3c
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-18 15:30:48 -0300

    Desafio 80 - WIP

--- Python Mundo 3- Desafio 80.py
+++ Python Mundo 3- Desafio 80.py
@@ -1,1 +1,32 @@
-Python Mundo 3- Desafio 79
+# 
+
+print('*' *30)
+print(f"{'Python Mundo 3 - Desafio 80' :^30}")
+print('*' *30)
+
+lista = []
+
+for c in range(0,5):
+    novoValor = int(input('Digite um valor'))
+    if len(lista) == 0:
+        print(f'{novoValor} é o primeiro valor inserido na lista')
+        lista.append(novoValor)
+    else:
+        listaMax = max(lista)
+        listaMin = min(lista)
+        print(f'Máximo {listaMax}')
+        print(f'Mínimo {listaMin}')
+        if novoValor>listaMax:
+            lista.append(novoValor)
+            print(f'O {novoValor} foi adicionado na posição {lista.index(novoValor)}')
+        if novoValor<listaMin:
+            lista.insert(0, novoValor)
+            print(f'O {novoValor} foi adicionado na posição {lista.index(novoValor)}')
+            
+        
+print(f'A lista inserida foi {lista}')
+
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
