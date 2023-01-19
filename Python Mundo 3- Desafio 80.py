7f20a4fa78085fa86ab5e344860a66b654d1a357
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-19 16:41:20 -0300

    Desafio 80 finalizado!

--- Python Mundo 3- Desafio 80.py
+++ Python Mundo 3- Desafio 80.py
@@ -1,4 +1,14 @@
-# 
+# Esse desafio também foi bem desafiador, mas a solução se mostrou mais simples que parecia ser 
+# no princípio. Construir um algoritmo que peça ao usuário para digitar 5 números e inserí-los
+# na posição correta dependendo de seu valor, sem usar sort ao final.
+
+# O usuário é solicitado a inserir um valor, que será testado, sendo o primeiro valor ele será
+# inserido no 'final' da lista, já que é o primeiro, valores maiores que o último serão coloca
+# dos depois do último valor, e o último valor continua sendo o maior de todos, se o valor não 
+# for maior que o maior valor da lista então é executado um teste que percorra toda a lista
+# e encontre um valor que seja menor que o novo valor, ou que o primeiro cujo valor seja maior 
+# que o novo valor, e então esse será a posição do novo valor, ao final o programa deve exibir 
+# a lista completa, que deve estar na ordem crescente correta.
 
 print('*' *30)
 print(f"{'Python Mundo 3 - Desafio 80' :^30}")
@@ -8,22 +18,17 @@ lista = []
 
 for c in range(0,5):
     novoValor = int(input('Digite um valor'))
-    if len(lista) == 0:
-        print(f'{novoValor} é o primeiro valor inserido na lista')
+    if c == 0 or novoValor > lista[-1]:
         lista.append(novoValor)
+        print(f'O valor {novoValor} foi adicionado no final da lista')
     else:
-        listaMax = max(lista)
-        listaMin = min(lista)
-        print(f'Máximo {listaMax}')
-        print(f'Mínimo {listaMin}')
-        if novoValor>listaMax:
-            lista.append(novoValor)
-            print(f'O {novoValor} foi adicionado na posição {lista.index(novoValor)}')
-        if novoValor<listaMin:
-            lista.insert(0, novoValor)
-            print(f'O {novoValor} foi adicionado na posição {lista.index(novoValor)}')
-            
-        
+        posicao=0
+        while posicao < len(lista):
+            if novoValor <= lista[posicao]:
+                lista.insert(posicao, novoValor)
+                print(f'O valor {novoValor} foi adicionado na posição {posicao}')
+                break
+            posicao +=1
 print(f'A lista inserida foi {lista}')
 
 
