ecfb33c2cea9be6cc34b9737c0dc7fcf4e530805
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-12-22 10:46:46 -0300

    Desafio 84

--- Python Mundo 3- Desafio 84.py
+++ Python Mundo 3- Desafio 84.py
@@ -1,41 +1,36 @@
+# Depois de pesquisar na documentação um pouco encontrei uma solução simples mas que não me deixou
+# satisfeito, mas antes feito que perfeito.
+
+# O sistema construído deve solicitar nome e peso de n pessoas, sempre perguntando ao usuário se ele
+# deseja continuar e solicitar as informações até que o usuário escolha não continuar a inserir novas
+# informações. O algoritmo reserva essa informações em uma lista prévia, lançando-as em uma lista final, 
+# resetando a lista prévia, contando quantas inserções foram feitas, e ao final da última iteração, 
+# o sistema avalia qual foram os pesos máximos e mínimos, sem alterar a ordem da lista final e imprimindo
+# na tela quantas inserções foram feitas, os pesos máximos e mínimos, com seus respectivos nomes e 
+# imprime a lista final por último.
+
+print('*' *30)
+print(f"{'Python Mundo 3 - Desafio 84' :^30}")
+print('*' *30)
+
 continuar = 's'
 listaFinal = list()
 listaPrevia = list()
-listaMais = list()
-listaMenos = list()
+
 contador = 0
 
 while continuar == 's':
     listaPrevia.append(str(input(f"Nome da pessoa número {contador}: ")))
     listaPrevia.append(int(input('Peso: ')))
-    if contador == 0:
-        menorPeso = listaPrevia[1]
-        menorNome = listaPrevia[0]
-        maiorPeso = listaPrevia[1]
-        maiorNome = listaPrevia[0]
-        print(f"A pessoa cadastra de maior peso foi {maiorNome} com {maiorPeso}Kg")
-        print(f"A pessoa cadastra de menor peso foi {menorNome} com {menorPeso}Kg")
-
-    if contador>=1:
-        if listaPrevia[1] <= menorPeso:
-            menorPeso = listaPrevia[1]
-            menorNome = listaPrevia[0]
-            listaMenos.append(listaPrevia[0])
-            listaMenos.append(listaPrevia[1])
-        if listaPrevia[1] >= maiorPeso:
-            maiorPeso = listaPrevia[1]
-            maiorNome = listaPrevia[0]
-            listaMais.append(listaPrevia[0])
-            listaMais.append(listaPrevia[1])
-        print(f"A pessoa cadastra de maior peso foi {maiorNome} com {maiorPeso}Kg")
-        print(f"A pessoa cadastra de menor peso foi {menorNome} com {menorPeso}Kg")
     listaFinal.append(listaPrevia[:])
     contador = contador + 1
     listaPrevia.clear()
     continuar = input('Quer continuar? [S/N]').lower()
     if continuar == 'n':
+        maior=max(listaFinal, key=lambda item: item[1])
+        menor=min(listaFinal, key=lambda item: item[1])
         break
 print(f"Foram cadastradas {contador} pessoas!")
-print(f"A pessoa cadastra de maior peso foi {listaMais}")
-print(f"A pessoa cadastra de menor peso foi {listaMenos}")
+print(f"O maior peso foi de {maior[0]}, com {maior[1]}kg.")
+print(f"O menor peso foi de {menor[0]}, com {menor[1]}kg.")
 print(listaFinal)
