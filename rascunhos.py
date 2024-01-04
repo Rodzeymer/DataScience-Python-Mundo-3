2b5e51ed43f96f4ed7b2d6b47e9f9086329bcec1
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2024-01-04 16:24:55 -0300

    desafio 93 rascunho

--- rascunhos.py
+++ rascunhos.py
@@ -1,15 +1,25 @@
-# desafio 91
+# desafio 93
 
-import random
-contador = 1
-jogadas = {}
-jogo = []
+# nome do jogador 
+# quantas partidas
+ #    quantos gols em cada jogador
+  #   total de gols
+# imprime gols/partidas
+gols = []
+partidas = {}
+jogador = {}
+totalGols = 0
 
-for sorteio in range (0,4):
-    jogadas['Jogador'] = contador
-    jogadas['Resultado'] = random.randint(1,6)
-    contador = contador + 1
-    jogo.append(jogadas.copy())
-print(jogadas)
-organizado = jogadas.
-print(organizado)
+jogador['Nome'] = str(input('Digite o nome do jogador'))
+numPartidas = int(input(f"Quantas partidas o jogador {jogador['Nome']} jogou nessa temporada?"))
+
+for c in range (1, numPartidas+1):
+    partidas['Partida [c]'] = int(input(f"Quantos gols o jogador {jogador['Nome']} marcou na partida {c}?"))
+    jogador['Gols'] = (partidas['Partida [c]'].copy())
+    totalGols = totalGols + partidas['Partida [c]']
+    partidas[c] = (f"Partida {c}: {partidas['Partida [c]']}")
+aproveitamento = totalGols/numPartidas
+print(jogador)
+print(f"Gols {gols}")
+print(totalGols)
+print(partidas)
