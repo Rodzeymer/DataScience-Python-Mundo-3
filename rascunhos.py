56d8240a3c82fd24b92bf29e94a52c615dba3991
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2024-01-07 12:04:30 -0300

    rascunho 95

--- rascunhos.py
+++ rascunhos.py
@@ -1,6 +1,6 @@
 # desafio 95
 
-Cadastro = []
+cadastro = []
 jogador = {}
 golsFeitos = []
 golTotal = 0
@@ -11,19 +11,31 @@ while True:
     for c in range(1, jogador['Partidas']+1):
         gols = int(input(f"Quantos gols {jogador['Nome']} fez no jogo {c}?"))
         golTotal = golTotal + gols
-        golsFeitos.append(gols)
-    continuuar = str(input(f"Quer continuar? [S/N]"))
-print(f"")
+    golsFeitos.append(golTotal)
+    jogador['GolsFeitos'] = golsFeitos
+    jogador['GolTotal'] = golTotal
+    jogador['Rendimento'] = jogador['GolTotal']/jogador['Partidas']
+    continuar = str(input(f"Quer continuar? [S/N]"))
 
-jogador['GolsFeitos'] = golsFeitos
-jogador['GolTotal'] = golTotal
-jogador['Rendimento'] = jogador['GolTotal']/jogador['Partidas']
+    cadastro.append(jogador.copy())
+    jogador.clear()
+    if continuar in 'Nn':
+        break
+print(cadastro)
+print(golsFeitos)
 
-print(f"O jogador {jogador['Nome']} participou de {jogador['Partidas']} jogos")
-for c in range(0, jogador['Partidas']):
-    print(f"{' ':4}=> No jogo {c+1} fez {golsFeitos[c]} gols ")
 
-print(f"Com um total de {jogador['GolTotal']} gols, seu rendimento é {jogador['Rendimento']:0.0f} gols por partida")
+
+quantidadeJogador = len(cadastro)
+#print(f"Foram cadastrados {cadastro} jogadores")
+print(f"-="*15)
+
+
+#print(f"O jogador {jogador['Nome']} participou de {jogador['Partidas']} jogos")
+#for c in range(0, jogador['Partidas']):
+#    print(f"{' ':4}=> No jogo {c+1} fez {golsFeitos[c]} gols ")
+
+#print(f"Com um total de {jogador['GolTotal']} gols, seu rendimento é {jogador['Rendimento']:0.0f} gols por partida")
 
 print('*' *30)
 print(f"{'FIM':^30}")
