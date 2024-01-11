fbc3965baf7b3e9c92f01fc3408369bf2d93e0bd
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2024-01-11 16:53:33 -0300

    rascunho 102

--- rascunhos.py
+++ rascunhos.py
@@ -1,8 +1,23 @@
-def linhas(mensagem):
-    tamanho = len(mensagem)+4
-    print('-'*tamanho)
-    print(f"  {mensagem:^4}  ")
-    print('-'*tamanho)
-    
-linhas('Mensagem1')
-linhas('Mensagem bem longa para ver até onde vai essa bagaça')
+# desafio 102
+
+from time import sleep
+
+def fatorial(numero = 1, show='N'):
+    f=1
+    for c in range(numero, 0, -1):
+        f*=c
+        sleep(0.5)
+        if show == 'S':
+            print(f"{c} ", end='')
+            if c > 1:
+               print("x ", end='')
+            if c == 1:
+                print(f"= {f}")
+                sleep(0.5)
+    if show == 'N':
+        print(f"{numero}! = {f}")
+numero = int(input('Digite um número a ser calculado seu fatorial'))
+show = str(input('Quer ver o processo? [S/N]')).upper()
+
+fatorial(numero, show)
+        
