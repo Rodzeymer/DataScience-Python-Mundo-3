a8b7d53332c345237ad75700b4b7bd664723810f
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2024-01-15 16:30:38 -0300

    Agora o 104 funciona

--- Python Mundo 3- Desafio 104.py
+++ Python Mundo 3- Desafio 104.py
@@ -1,28 +1,37 @@
-# Tentando tratamento de erros
+# Tentando tratamento de erros, mas no final foi só pegar a entrada como string e testar se
+# ela é numérica com a função isnumeric, tentei de outra forma mas essa foi o jeito que deu 
+# certo
+
+# O usuário simplesmente digita algo e manda para a função, sendo um dado numérico inteiro ele
+# aceita a informação, se não ele exibe uma mensagem de erro e solicita novamente a entrada
+# do usuário.
 
 
 print('*' *30)
 print(f"{'Python Mundo 3 - Desafio 104' :^30}")
 print('*' *30)
 
-def leiaInt(entrada):
+def leiaInt(digito):
     """Pega a entrada inserida pelo usuário e tenta transformar em integer, e verifica e trata
     o erro, re-iniciando o algoritmo se mão conseguir transformar
     :entrada: entrada são tipada do usuário
     :return: Se foi inserido um valor numérico inteiro ou não
     """
+    ok = False
+    valor = 0
     while True:
-        try:
-            numero = int(entrada)
-            if type(numero) == int:
-                print('Entrada ok')
-                break
-            else:
-                print('Entrada não ok')
-        except ValueError as error:
-            print('Deu erro de valor')
-entrada = input('Digite algo')
-leiaInt(entrada)
+        n=str(input(digito))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print("\033[0;31mErro Digite um número inteiro válido\033[m")
+        if ok:
+            break
+    return valor
+
+n = leiaInt('Digite um número: ')
+print(f"Você acabou de digitar o número {n}!")
 
 print('*' *30)
 print(f"{'FIM':^30}")
